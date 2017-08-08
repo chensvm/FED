a2e8b53cc3fa1f722d17a6bc7665f72ec1452964
Nancy Cheng <nancy.cheng.tl@gmail.com>
2017-08-08 10:04:25 +0800

    start ann

--- ANN/ANN.py
+++ ANN/ANN.py
@@ -1,57 +1,191 @@
-# use natural language toolkit
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 import os
 import json
 import datetime
+import numpy as np
+import time
+import re
+from datetime import timedelta, date
+import csv
+from nltk.corpus import stopwords
+
 stemmer = LancasterStemmer()
 
+
+def loadDataSet():
+
+    print "start loading training dataset"
+    postingList = []
+    classVec = []
+    documents = []
+    posNum = 0
+    negNum = 0
+    neutralNum = 0
+
+    with open('../fed_rates/fed_date_rate_training.csv', 'r') as c1:
+        reader = csv.reader(c1)
+        next(reader, None)  # skip the headers
+        prevRow = "2004-11-01"
+        for row in reader:
+
+            if row[1] == "1":
+
+                cur_year, cur_month, cur_day = str(row[0]).split("-")
+                pre_year, pre_month, pre_day = prevRow.split("-")
+
+                start_date = date(int(pre_year), int(pre_month), int(pre_day))
+                end_date = date(int(cur_year), int(cur_month), int(cur_day))
+
+                for single_date in daterange(start_date, end_date):
+
+
+                    with open('../data/filtered_articles/nytimes/' +str(single_date.strftime("%Y"))+"/"+ str(single_date.strftime("%Y%m%d")) + ".npy", 'r') as myfile:
+
+                        print str(single_date.strftime("%Y-%m-%d"))
+                        data = np.load(myfile)
+                        if data.size == 0:
+                            pass
+
+                        else:
+                            for news in data:
+                                regEx = re.compile('\\W*')
+                                listOfTokens = regEx.split(news)
+                                listOfTokens = [tok.lower().encode('utf-8') for tok in listOfTokens if len(tok) > 0]
+                                filtered_words = [word for word in listOfTokens if word not in stopwords.words('english')]
+
+                                posNum += 1
+                                #準備posting list
+                                postingList.append(filtered_words)
+                                # 同時建立一個分類向量搭配
+                                classVec.append(1)
+                                documents.append(filtered_words, 1)
+
+                    myfile.close()
+
+            elif row[1] == "-1":
+                cur_year, cur_month, cur_day = str(row[0]).split("-")
+                pre_year, pre_month, pre_day = prevRow.split("-")
+
+                start_date = date(int(pre_year), int(pre_month), int(pre_day))
+                end_date = date(int(cur_year), int(cur_month), int(cur_day))
+
+                for single_date in daterange(start_date, end_date):
+
+                    with open('../data/filtered_articles/nytimes/' + str(single_date.strftime("%Y")) + "/" + str(
+                            single_date.strftime("%Y%m%d")) + ".npy", 'r') as myfile:
+                        print str(single_date.strftime("%Y-%m-%d"))
+
+                        data = np.load(myfile)
+
+                        if data.size == 0:
+                            pass
+
+                        else:
+
+                            for news in data:
+
+                                regEx = re.compile('\\W*')
+                                listOfTokens = regEx.split(news)
+                                listOfTokens = [tok.lower().encode('utf-8') for tok in listOfTokens if len(tok) > 0]
+                                filtered_words = [word for word in listOfTokens if word not in stopwords.words('english')]
+
+                                negNum += 1
+                                postingList.append(filtered_words)
+                                classVec.append(-1)
+                                documents.append(filtered_words, -1)
+
+                    myfile.close()
+
+            elif row[1] == "0":
+                cur_year, cur_month, cur_day = str(row[0]).split("-")
+                pre_year, pre_month, pre_day = prevRow.split("-")
+
+                start_date = date(int(pre_year), int(pre_month), int(pre_day))
+                end_date = date(int(cur_year), int(cur_month), int(cur_day))
+
+                for single_date in daterange(start_date, end_date):
+
+                    with open('../data/filtered_articles/nytimes/' + str(single_date.strftime("%Y")) + "/" + str(
+                            single_date.strftime("%Y%m%d")) + ".npy", 'r') as myfile:
+
+                        print str(single_date.strftime("%Y-%m-%d"))
+
+                        data = np.load(myfile)
+
+                        if data.size == 0:
+                            pass
+
+                        else:
+
+                            for news in data:
+
+                                regEx = re.compile('\\W*')
+                                listOfTokens = regEx.split(news)
+                                listOfTokens = [tok.lower().encode('utf-8') for tok in listOfTokens if len(tok) > 0]
+                                filtered_words = [word for word in listOfTokens if word not in stopwords.words('english')]
+
+                                neutralNum += 1
+                                postingList.append(filtered_words)
+                                classVec.append(0)
+
+                        documents.append((postingList, pattern['class']))
+
+                    myfile.close()
+
+            else:
+                print "pass posting list error: "+ row[0]
+                pass
+
+            prevRow = row[0]
+
+    print "finish postingList"
+
+    return postingList, classVec
 # 3 classes of training data
-training_data = []
-training_data.append({"class":"greeting", "sentence":"how are you?"})
-training_data.append({"class":"greeting", "sentence":"how is your day?"})
-training_data.append({"class":"greeting", "sentence":"good day"})
-training_data.append({"class":"greeting", "sentence":"how is it going today?"})
+#training_data = []
 
-training_data.append({"class":"goodbye", "sentence":"have a nice day"})
-training_data.append({"class":"goodbye", "sentence":"see you later"})
-training_data.append({"class":"goodbye", "sentence":"have a nice day"})
-training_data.append({"class":"goodbye", "sentence":"talk to you soon"})
 
-training_data.append({"class":"sandwich", "sentence":"make me a sandwich"})
-training_data.append({"class":"sandwich", "sentence":"can you make a sandwich?"})
-training_data.append({"class":"sandwich", "sentence":"having a sandwich today?"})
-training_data.append({"class":"sandwich", "sentence":"what's for lunch?"})
-print ("%s sentences in training data" % len(training_data))
+
+# training_data.append({"class":"greeting", "sentence":"how are you?"})
+
+#print ("%s sentences in training data" % len(training_data))
+
+
+# We can now organize our data structures for documents, classes and words.
 
 words = []
 classes = []
 documents = []
-ignore_words = ['?']
+# ignore_words = ['?']
 # loop through each sentence in our training data
-for pattern in training_data:
-    # tokenize each word in the sentence
-    w = nltk.word_tokenize(pattern['sentence'])
-    # add to our words list
-    words.extend(w)
-    # add to documents in our corpus
-    documents.append((w, pattern['class']))
-    # add to our classes list
-    if pattern['class'] not in classes:
-        classes.append(pattern['class'])
+# for pattern in training_data:
+#     # tokenize each word in the sentence
+#     w = nltk.word_tokenize(pattern['sentence'])
+#     # add to our words list
+#     words.extend(w)
+#     # add to documents in our corpus
+#     documents.append((w, pattern['class']))
+#     # add to our classes list
+#     if pattern['class'] not in classes:
+#         classes.append(pattern['class'])
 
 # stem and lower each word and remove duplicates
-words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
-words = list(set(words))
+# words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
+# words = list(set(words))
+#
+# # remove duplicates
+# classes = list(set(classes))
 
-# remove duplicates
-classes = list(set(classes))
+classes, words = loadDataSet()
 
 print (len(documents), "documents")
 print (len(classes), "classes", classes)
 print (len(words), "unique stemmed words", words)
 
 # create our training data
+# Our training data is transformed into “bag of words”
+
 training = []
 output = []
 # create an empty array for our output
@@ -81,3 +215,174 @@ w = documents[i][0]
 print ([stemmer.stem(word.lower()) for word in w])
 print (training[i])
 print (output[i])
+
+
+# compute sigmoid nonlinearity
+# The Sigmoid function, which describes an S shaped curve.
+# We pass the weighted sum of the inputs through this function to
+# normalise them between 0 and 1.
+
+def sigmoid(x):
+    output = 1 / (1 + np.exp(-x))
+    return output
+
+
+# convert output of sigmoid function to its derivative
+# The derivative of the Sigmoid function.
+# This is the gradient of the Sigmoid curve.
+# It indicates how confident we are about the existing weight.
+# Sigmoid derivative’ tells us about the slope of the curve on any point
+
+def sigmoid_output_to_derivative(output):
+    return output * (1 - output)
+
+
+def clean_up_sentence(sentence):
+    # tokenize the pattern
+    sentence_words = nltk.word_tokenize(sentence)
+    # stem each word
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    return sentence_words
+
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+def bow(sentence, words, show_details=False):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words
+    bag = [0] * len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+
+    return (np.array(bag))
+
+
+def think(sentence, show_details=False):
+    x = bow(sentence.lower(), words, show_details)
+    if show_details:
+        print ("sentence:", sentence, "\n bow:", x)
+    # input layer is our bag of words
+    l0 = x
+    # matrix multiplication of input and hidden layer
+    l1 = sigmoid(np.dot(l0, synapse_0))
+    # output layer
+    l2 = sigmoid(np.dot(l1, synapse_1))
+    return l2
+
+alphas = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
+# 0.001, 0.01 usually can not converge
+
+
+# ANN and Gradient Descent code from https://iamtrask.github.io//2015/07/27/python-network-part2/
+# Adjusting the synaptic weights each time.
+
+def train(X, y, hidden_neurons=10, alpha=1, epochs=50000, dropout=False, dropout_percent=0.5):
+    # self, training_set_inputs, training_set_outputs, number_of_training_iterations
+
+    print ("Training with %s neurons, alpha:%s, dropout:%s %s" % (
+    hidden_neurons, str(alpha), dropout, dropout_percent if dropout else ''))
+    print ("Input matrix: %sx%s    Output matrix: %sx%s" % (len(X), len(X[0]), 1, len(classes)))
+    np.random.seed(1)
+    # randomly generate a start point
+
+    last_mean_error = 1
+    # randomly initialize our weights with mean 0
+    synapse_0 = 2 * np.random.random((len(X[0]), hidden_neurons)) - 1
+    synapse_1 = 2 * np.random.random((hidden_neurons, len(classes))) - 1
+
+
+    prev_synapse_0_weight_update = np.zeros_like(synapse_0)
+    prev_synapse_1_weight_update = np.zeros_like(synapse_1)
+
+    synapse_0_direction_count = np.zeros_like(synapse_0)
+    synapse_1_direction_count = np.zeros_like(synapse_1)
+
+    # we can train in different alpha
+
+    for j in iter(range(epochs + 1)):# repeat untill slope ==0
+
+        # Feed forward through layers 0, 1, and 2
+        layer_0 = X
+        layer_1 = sigmoid(np.dot(layer_0, synapse_0))
+        #computing error
+
+        if (dropout):
+            layer_1 *= np.random.binomial([np.ones((len(X), hidden_neurons))], 1 - dropout_percent)[0] * (
+            1.0 / (1 - dropout_percent))
+
+        layer_2 = sigmoid(np.dot(layer_1, synapse_1))
+
+        # how much did we miss the target value?
+        # Calculate the error (The difference between the desired output
+        # and the predicted output).
+        layer_2_error = y - layer_2
+
+        # Multiply the error by the input and again by the gradient of the Sigmoid curve.
+        # This means less confident weights are adjusted more.
+        # This means inputs, which are zero, do not cause changes to the weights.
+        # adjustment = dot(training_set_inputs.T, error * self.__sigmoid_derivative(output))
+
+
+        # Adjust the weights.
+        if (j % 10000) == 0 and j > 5000:
+            # if this 10k iteration's error is greater than the last iteration, break out
+            if np.mean(np.abs(layer_2_error)) < last_mean_error:
+                print ("delta after " + str(j) + " iterations:" + str(np.mean(np.abs(layer_2_error))))
+                last_mean_error = np.mean(np.abs(layer_2_error))
+            else:
+                print ("break:", np.mean(np.abs(layer_2_error)), ">", last_mean_error)
+                break
+
+        # in what direction is the target value?
+        # were we really sure? if so, don't change too much.
+        #optimize to reduce error
+        layer_2_delta = layer_2_error * sigmoid_output_to_derivative(layer_2)
+
+        # how much did each l1 value contribute to the l2 error (according to the weights)?
+        layer_1_error = layer_2_delta.dot(synapse_1.T)
+
+        # in what direction is the target l1?
+        # were we really sure? if so, don't change too much.
+        layer_1_delta = layer_1_error * sigmoid_output_to_derivative(layer_1)
+
+        # calculate current slope at X position
+
+
+        synapse_1_weight_update = (layer_1.T.dot(layer_2_delta))
+        synapse_0_weight_update = (layer_0.T.dot(layer_1_delta))
+
+        if (j > 0):
+            synapse_0_direction_count += np.abs(
+                ((synapse_0_weight_update > 0) + 0) - ((prev_synapse_0_weight_update > 0) + 0))
+            synapse_1_direction_count += np.abs(
+                ((synapse_1_weight_update > 0) + 0) - ((prev_synapse_1_weight_update > 0) + 0))
+
+        # tune model with alpha
+        synapse_1 += alpha * synapse_1_weight_update
+        synapse_0 += alpha * synapse_0_weight_update
+
+        prev_synapse_0_weight_update = synapse_0_weight_update
+        prev_synapse_1_weight_update = synapse_1_weight_update
+
+    now = datetime.datetime.now()
+
+    # persist synapses
+    synapse = {'synapse0': synapse_0.tolist(), 'synapse1': synapse_1.tolist(),
+               'datetime': now.strftime("%Y-%m-%d %H:%M"),
+               'words': words,
+               'classes': classes
+               }
+    synapse_file = "synapses.json"
+
+    with open(synapse_file, 'w') as outfile:
+        json.dump(synapse, outfile, indent=4, sort_keys=True)
+    print ("saved synapses to:", synapse_file)
+
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
