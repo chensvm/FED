6b2fcc363fcb7c087eabe6182745481aa40defca
Nancy Cheng <nancy.cheng.tl@gmail.com>
2017-10-13 12:05:24 +0800

    start to work on DARNN

--- tool/readPickle.py
+++ tool/readPickle.py
@@ -4,7 +4,21 @@ import pickle
 import os
 
 df = pd.read_pickle("../eikon_sample_data/timeseries_sample1.pickle")
-df.head()
+print ("time series:")
+print (df.head())
+
+df = pd.read_pickle('../eikon_sample_data/fs_sample.pickle')
+print ("finance statement:")
+print (df)
+
+df = pd.read_pickle('../eikon_sample_data/news_sample.pickle')
+print ("story ID:")
+print (df.storyId.iloc[0])
+print ("story_head:")
+print (df.text.iloc[0])
+print ("story content with html tag")
+print (df.story_content.iloc[0])
+
 
 # if __name__ == '__main__':
 #     main()
